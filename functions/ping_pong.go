5dcbf77d5a9535096b05b0bd7c665eccb5e35a63
arontaubyte <asd@asd.com>
2023-01-04 11:52:03 -0600

    as

--- functions/ping_pong.go
+++ functions/ping_pong.go
@@ -1,14 +1,33 @@
 package lib
 
 import (
+	"fmt"
+
 	"github.com/taubyte/go-sdk/event"
 	"bitbucket.org/taubyte/go-sdk/database"
 )
 
 //export ping
 func ping(e event.Event) uint32 {
-	database, err := database.New("failed_database")
+	h, err := e.HTTP()
+	if err != nil {
+		return 1
+	}
+
+	err = testDatabase(h)
+	if err != nil {
+		h.Write([]byte(fmt.Sprintf("Failed: %s", err.Error())))
+		return 1
+	}
+
+	return 0
+}
+
+func testDatabase(e event.Event) uint32{
+	_database, err := database.New("Chat_Database")
 	if err != nil {
 		return err
 	}
+
+	fmt.Println(_database)
 }
